0f32f573f483105e264ff1eeaeae8978d31c0376
hamid321 <hamidazim321@gmail.com>
2023-11-30 18:18:32 +0500

    Fix linter errors

--- myList.rb
+++ myList.rb
@@ -1,15 +1,19 @@
-require_relative 'my_enumerable.rb'
+# frozen_string_literal: true
+
+require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
   attr_reader :list
+
   def initialize(*elements)
     @list = elements
   end
+
   def each(&block)
     @list.each(&block)
   end
 end
 
-list = MyList.new(1,2,3,4,5)
+list = MyList.new(1, 2, 3, 4, 5)
 
-puts list.any? {|item| item >= 5}
+puts(list.any? { |item| item >= 5 })
--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module MyEnumerable
   def any?
-    each {|item| return true if yield(item)}
+    each { |item| return true if yield(item) }
     false
   end
-  def all?
-  end
+
+  def all?; end
+
   def filter(&block)
     @list.select(&block)
   end
